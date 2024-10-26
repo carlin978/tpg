5afe4fa109a90e387e9d3d226a6c09a54c8e1f63
carlin978 <carlin978@proton.me>
2024-10-26 19:46:31 +0100

    Attempt to support encrypting files

--- src/crypt.rs
+++ src/crypt.rs
@@ -7,14 +7,14 @@ use pgp::{Deserializable, SignedPublicKey};
 use rand::thread_rng;
 use smallvec::*;
 
-pub fn gen_priv_key(name: &str, email: &str) -> SignedSecretKey {
+pub fn gen_key(name: &str, email: &str) -> SignedSecretKey {
     let mut key_params = SecretKeyParamsBuilder::default();
     key_params
-        .key_type(KeyType::Rsa(2048))
+        .key_type(KeyType::Rsa(4096))
         .can_certify(false)
         .can_sign(true)
         .can_encrypt(true)
-        .primary_user_id(format!("{} <{}>", name, email).into())
+        .primary_user_id(format!("{} <{}>", name, email))
         .preferred_symmetric_algorithms(smallvec![SymmetricKeyAlgorithm::AES256])
         .preferred_hash_algorithms(smallvec![HashAlgorithm::SHA2_256])
         .preferred_compression_algorithms(smallvec![CompressionAlgorithm::ZLIB]);
@@ -39,13 +39,36 @@ pub fn read_priv_key(data: Vec<u8>) -> Result<SignedSecretKey, pgp::errors::Erro
     SignedSecretKey::from_bytes(data.as_slice())
 }
 
+pub fn read_pub_key(data: Vec<u8>) -> Result<SignedPublicKey, pgp::errors::Error> {
+    SignedPublicKey::from_bytes(data.as_slice())
+}
+
 pub fn encrypt_text_to_binary(key: SignedPublicKey, text: String) -> Result<Vec<u8>, String> {
-    let encrypted = match key.encrypt(thread_rng(), text.as_bytes(), EskType::V3_4) {
-        Ok(val) => val,
-        Err(err) => return Err(err.to_string()),
-    };
-    match encrypted {
-        PkeskBytes::Rsa { mpi } => Ok(mpi.as_bytes().into()),
-        _ => Err("Unknown encryption method".into()),
+    let encrypted_result = key.encrypt(thread_rng(), text.as_bytes(), EskType::V6);
+    if let Ok(encrypted) = encrypted_result {
+        match encrypted {
+            PkeskBytes::Rsa { mpi } => Ok(mpi.as_bytes().into()),
+            _ => Err("Unknown/Non-supported encryption method".into()),
+        }
+    } else {
+        Err(format!(
+            "Failed to Encrypt: {}",
+            encrypted_result.unwrap_err().to_string()
+        ))
+    }
+}
+
+pub fn encrypt_bytes_to_binary(key: SignedPublicKey, bytes: Vec<u8>) -> Result<Vec<u8>, String> {
+    let encrypted_result = key.encrypt(thread_rng(), bytes.as_slice(), EskType::V3_4);
+    if let Ok(encrypted) = encrypted_result {
+        match encrypted {
+            PkeskBytes::Rsa { mpi } => Ok(mpi.as_bytes().into()),
+            _ => Err("Unknown/Non-supported encryption method".into()),
+        }
+    } else {
+        Err(format!(
+            "Failed to Encrypt: {}",
+            encrypted_result.unwrap_err().to_string()
+        ))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,13 @@
 //imports
 use clap::Parser;
 use colored::Colorize;
-use pgp::{
-    ser::Serialize,
-    types::{EskType, PkeskBytes, PublicKeyTrait},
-};
-use rand::thread_rng;
+use pgp::ser::Serialize;
 use std::{env, fs};
 //modules
 mod cli;
 use cli::{Cli, Commands};
 mod crypt;
-use crypt::{encrypt_text_to_binary, gen_priv_key, read_priv_key};
+use crypt::{encrypt_bytes_to_binary, encrypt_text_to_binary, gen_key, read_priv_key};
 
 fn main() {
     let cwd = env::current_dir().unwrap();
@@ -34,7 +30,7 @@ fn main() {
             armor,
             output,
         } => {
-            let key = gen_priv_key(name, email);
+            let key = gen_key(name, email);
             let mut file_path = cwd.clone();
             file_path.push(output);
             println!("Path: {}", file_path.display());
@@ -61,20 +57,45 @@ fn main() {
             file_path.push(key_file);
             println!("Key: {}", file_path.display());
             if file_path.exists() {
-                let data = fs::read(file_path.as_path()).expect("Failed to read file");
-                let key = read_priv_key(data).expect("Failed to read private key");
+                let data = fs::read(file_path.as_path()).expect("Failed to read key file");
+                let key = read_priv_key(data).expect("Failed to read key");
                 if *armor {
                     todo!("Armor not implemented");
                 } else {
-                    match encrypt_text_to_binary(key.into(), text.clone().unwrap()) {
-                        Ok(encrypted) => {
-                            let mut file_path = cwd.clone();
-                            file_path.push(output);
-                            println!("Output: {}", file_path.display());
-                            fs::write(file_path.as_path(), encrypted.as_slice())
-                                .expect("Failed to Write file");
+                    if let Some(input_path) = input {
+                        todo!("Broken");
+                        // let mut file_path = cwd.clone();
+                        // file_path.push(input_path);
+                        // if file_path.exists() {
+                        //     let input_content =
+                        //         fs::read(file_path).expect("Failed to read input file");
+                        //     match encrypt_bytes_to_binary(key.into(), input_content) {
+                        //         Ok(encrypted) => {
+                        //             let mut file_path = cwd.clone();
+                        //             file_path.push(output);
+                        //             println!("Output: {}", file_path.display());
+                        //             fs::write(file_path.as_path(), encrypted.as_slice())
+                        //                 .expect("Failed to Write file");
+                        //         }
+                        //         Err(err) => println!("{}", err.red()),
+                        //     }
+                        // }
+                    } else if let Some(text) = text {
+                        match encrypt_text_to_binary(key.into(), text.clone()) {
+                            Ok(encrypted) => {
+                                let mut file_path = cwd.clone();
+                                file_path.push(output);
+                                println!("Output: {}", file_path.display());
+                                fs::write(file_path.as_path(), encrypted.as_slice())
+                                    .expect("Failed to Write file");
+                            }
+                            Err(err) => println!("{}", err.red()),
                         }
-                        Err(err) => println!("{}", err.red()),
+                    } else {
+                        println!(
+                            "{}",
+                            "You must pass either an input file or text to be encrypted".red()
+                        );
                     }
                 }
             }
